a3e4afa47d9495188b754810097911629275ce51
Sherif <sherif.mohamed@mcledger.co>
2021-11-24 20:36:55 +0200

    feat(init): init news app with needed packages

---
+++ App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  Text,
+  View,
+} from 'react-native';
+
+export default function App() {
+  return (
+    <SafeAreaView>
+    <ScrollView>
+      <View>
+        <Text>test</Text>
+      </View>
+    </ScrollView>
+  </SafeAreaView>
+  );
+}
